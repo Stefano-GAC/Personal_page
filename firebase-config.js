b741b36eab475401b0f5eb54fdc9ae82e2a1b23d
Stefano-GAC <sac0024@alu.medac.es>
2026-04-19 18:29:30 -0500

    feat(firebase): agregar configuracion real del proyecto bd-personalpage

--- firebase-config.js
+++ firebase-config.js
@@ -8,12 +8,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/fireba
 import { getAuth }       from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey:            "TU_API_KEY",
-  authDomain:        "TU_PROYECTO.firebaseapp.com",
-  projectId:         "TU_PROYECTO",
-  storageBucket:     "TU_PROYECTO.appspot.com",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId:             "TU_APP_ID"
+  apiKey:            "AIzaSyDkJeMa19ZpjYh5OWMfee1v92oSdq_HyMQ",
+  authDomain:        "bd-personalpage.firebaseapp.com",
+  projectId:         "bd-personalpage",
+  storageBucket:     "bd-personalpage.firebasestorage.app",
+  messagingSenderId: "367658988704",
+  appId:             "1:367658988704:web:5968f82a09d5b036ab85c5",
+  measurementId:     "G-NM4PXBWTVE"
 };
 
 const app  = initializeApp(firebaseConfig);
